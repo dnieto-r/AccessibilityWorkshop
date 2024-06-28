ceae058477249b3211b42c8fe120907b8a81702d
Daniel <daniel.nietorodriguez@telefonica.com>
2024-06-28 13:10:51 +0200

    fix: add scroll in menu

--- app/src/main/java/com/movistarhome/accessibilityworkshop/ui/workshopSelector/WorkshopSelector.kt
+++ app/src/main/java/com/movistarhome/accessibilityworkshop/ui/workshopSelector/WorkshopSelector.kt
@@ -1,12 +1,15 @@
 package com.movistarhome.accessibilityworkshop.ui.workshopSelector
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -30,8 +33,9 @@ fun WorkshopSelectorScreen(navController: NavController) {
     Column(
         Modifier
             .padding(vertical = 30.dp)
+            .verticalScroll(rememberScrollState())
             .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally) {
         Button(
             modifier = Modifier.semantics (mergeDescendants = true) { },
